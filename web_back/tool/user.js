0f9b03b1aaa743739562d9a068554855c2489116
xiaojian80231 <54383677+xiaojian80231@users.noreply.github.com>
2019-10-14 11:11:20 +0800

    ‘个人中心’调整

--- web_back/tool/user.js
+++ web_back/tool/user.js
@@ -19,12 +19,33 @@ var user = {
         });
     },
 
-    //getuser方法用来管理用户的简单信息获取接口的操作方式
+    //getInfo方法用来管理用户的简单信息获取接口的操作方式
     getInfo: function(options) {
         $.ajax({
             url: USER_INFO,
             success: options.callback
         });
+    },
+
+    //getAllInfo方法用来管理用户的详细信息接口的操作方式
+    getAllInfo: function(options) {
+        $.ajax({
+            url: USER_INFO_GET,
+            success: options.callback
+        });
+    },
+
+    //edit方法用来管理用户的编辑接口的操作方式
+    edit: function(options) {
+        $.ajax({
+            type: 'post',
+            url: USER_INFO_EDIT,
+            data: options.data, //获取options中的数据
+            contentType: false,
+            processData: false,
+            success: options.callback //获取options中的回调函数
+        });
+
     }
 
-}
+};
